78df5b901be9b8d7628b09f3fba414facb2e2069
suzukazu696969 <rugurethureira@gmail.com>
2022-12-09 17:16:50 +0900

    Issue6修正完成

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -41,7 +41,7 @@ class CalendarsController < ApplicationController
       end
 
 
-      days = { :month => (@todays_date + x).month, :date => (@todays_date + x).day, :plans => today_plans, :wday => wdays }
+      days = { :month => (@todays_date + x).month, :date => (@todays_date + x).day, :plans => today_plans, :wday => wdays[x] }
 
  
 
